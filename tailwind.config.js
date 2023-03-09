bc45d0b32cc58aafb7400ad0b4eccd025897c4c3
Hassan-Wanas <test@gamil.com>
2023-03-09 11:20:43 +0200

    extend tailwind.config & add fontawesome

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,21 @@ module.exports = {
     './index.html'
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        LightGray: '#BDBDBD',
+        Gray1: '#333333',
+        Gray2: '#4F4F4F',
+        Gray3: '#828282',
+        Gray4: '#4E5150',
+        Gray5: '#E0E0E0',
+        Gray6: '#F2F2F2',
+        Orange1: '#F2994A',
+      },
+    },
+    fontFamily: {
+      Montserrat: 'Montserrat',
+    },
   },
   plugins: [],
 }
